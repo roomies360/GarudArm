e77b026389a504dab0e1a78929cb1c8f86786b1b
roomies360 <roomiesconnect360@gmail.com>
2025-10-07 03:15:10 +0530

    connect to host websocket

--- garudScript.js
+++ garudScript.js
@@ -43,7 +43,7 @@ const connectingText = document.getElementById("connectContainer");
             if (ws && ws.readyState !== WebSocket.CLOSED) {
                 ws.close();
             }
-            ws = new WebSocket(`ws://10.119.19.214:81/`);
+            ws = new WebSocket(`ws://{window.location.host}/`);
             ws.onopen = () => {
                 console.log("WebSocket connected");
                 refreshBtn.classList.remove("button-disabled");
@@ -250,3 +250,4 @@ const connectingText = document.getElementById("connectContainer");
             holdBtn.classList.remove("active");
             sendWSMessage("GRIPPER", `${ch}:PLACE`);
         });
+
