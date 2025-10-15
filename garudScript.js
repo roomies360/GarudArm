c20b44225106e064459052479a4cffa2bfdf383e
roomies360 <roomiesconnect360@gmail.com>
2025-10-16 03:28:27 +0530

    Update garudScript.js

--- garudScript.js
+++ garudScript.js
@@ -6,7 +6,7 @@
             if (ws && ws.readyState !== WebSocket.CLOSED) {
                 ws.close();
             }
-            ws = new WebSocket(`ws://${window.location.host}/`);
+            ws = new WebSocket(`ws://${window.location.host}:81/`);
             ws.onopen = () => {
                 console.log("WebSocket connected");
                 refreshBtn.classList.remove("button-disabled");
@@ -299,3 +299,4 @@
             holdBtn.classList.remove("active");
             sendWSMessage("GRIPPER", `${ch}:PLACE`);
         });
+
